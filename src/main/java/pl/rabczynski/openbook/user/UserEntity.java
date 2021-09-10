bc8078edaf1355043ff299ebb96396a4ea77c106
Filip Rabczynski <f.rabczynski@gmail.com>
2021-09-10 20:52:12 +0200

    adding user entity and database script in charge of creating it

---
+++ src/main/java/pl/rabczynski/openbook/user/UserEntity.java
@@ -0,0 +1,39 @@
+package pl.rabczynski.openbook.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "users")
+@Table(name = "users")
+class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    @Column(unique = true)
+    @Email
+    private String email;
+
+}
