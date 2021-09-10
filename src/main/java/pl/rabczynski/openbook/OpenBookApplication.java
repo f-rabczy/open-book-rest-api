7b67c88bd58fe9ab8e759566e3a0bc14e024379f
Filip Rabczynski <f.rabczynski@gmail.com>
2021-09-10 20:07:56 +0200

    adding bean with RestTemplate

--- src/main/java/pl/rabczynski/openbook/OpenBookApplication.java
+++ src/main/java/pl/rabczynski/openbook/OpenBookApplication.java
@@ -2,12 +2,28 @@ package pl.rabczynski.openbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 public class OpenBookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OpenBookApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(List<HttpMessageConverter<?>> messageConverters) {
+        return new RestTemplate(messageConverters);
+    }
+
+    @Bean
+    public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter() {
+        return new ByteArrayHttpMessageConverter();
     }
 
 }
