23bd8d5052040a0342a2df9420664a0f46979bc7
Filip Rabczynski <f.rabczynski@gmail.com>
2021-09-27 22:40:50 +0200

    adding reviews, ratings and bookshelves fields to user entity

--- src/main/java/pl/rabczynski/openbook/user/domain/UserEntity.java
+++ src/main/java/pl/rabczynski/openbook/user/domain/UserEntity.java
@@ -3,15 +3,21 @@ package pl.rabczynski.openbook.user.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pl.rabczynski.openbook.book.domain.BookRatingEntity;
+import pl.rabczynski.openbook.book.domain.BookReviewEntity;
+import pl.rabczynski.openbook.book.domain.BookShelfEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -21,6 +27,7 @@ import javax.validation.constraints.NotNull;
 public class UserEntity {
 
     @Id
+    @NotNull
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
@@ -35,5 +42,14 @@ public class UserEntity {
     @Column(unique = true)
     @Email
     private String email;
+
+    @OneToMany(mappedBy = "rater")
+    private Set<BookRatingEntity> booksRatings = new HashSet<>();
+
+    @OneToMany(mappedBy = "reviewer")
+    private Set<BookReviewEntity> booksReviews = new HashSet<>();
+
+    @OneToMany(mappedBy = "user")
+    private Set<BookShelfEntity> bookShelves = new HashSet<>();
 
 }
