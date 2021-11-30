373a028cf78cfb02691d1e0d77fb8eb3a9c8c6e7
Filip Rabczynski <f.rabczynski@gmail.com>
2021-11-30 22:27:20 +0100

    changing package

--- src/main/java/pl/rabczynski/openbook/author/mapper/AuthorMapper.java
+++ src/main/java/pl/rabczynski/openbook/author/mapper/AuthorMapper.java
@@ -1,7 +1,7 @@
 package pl.rabczynski.openbook.author.mapper;
 
 import org.mapstruct.Mapper;
-import pl.rabczynski.openbook.author.AuthorEntity;
+import pl.rabczynski.openbook.author.domain.AuthorEntity;
 import pl.rabczynski.openbook.author.dto.AuthorDTO;
 
 @Mapper(componentModel = "spring")
