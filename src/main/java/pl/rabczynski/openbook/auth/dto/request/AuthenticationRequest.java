8a9c42f5cca8f4d9382acd1b75b5a29d40e8a03e
Filip Rabczynski <f.rabczynski@gmail.com>
2021-11-30 22:25:27 +0100

    adding dtos for authenticate

---
+++ src/main/java/pl/rabczynski/openbook/auth/dto/request/AuthenticationRequest.java
@@ -0,0 +1,14 @@
+package pl.rabczynski.openbook.auth.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/pl/rabczynski/openbook/auth/dto/response/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package pl.rabczynski.openbook.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+
+    private String jwt;
+}
