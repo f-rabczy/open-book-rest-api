691c4881a492304764f7ca595c8020cbe95cc809
Filip Rabczynski <f.rabczynski@gmail.com>
2022-01-24 22:02:41 +0100

    replacing usage of Arrays.asList with Collections.singletonList for one element array

--- src/main/java/pl/rabczynski/openbook/config/OpenBookConfig.java
+++ src/main/java/pl/rabczynski/openbook/config/OpenBookConfig.java
@@ -11,7 +11,7 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -21,8 +21,8 @@ public class OpenBookConfig {
     @Bean
     public Docket swaggerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("pl.rabczynski.openbook"))
                 .build();
@@ -40,6 +40,6 @@ public class OpenBookConfig {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
     }
 }
