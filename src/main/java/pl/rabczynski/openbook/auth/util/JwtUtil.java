95eba05d1c4b9a3aa3445c10897757e7038dde27
Filip Rabczynski <f.rabczynski@gmail.com>
2022-01-27 22:01:25 +0100

    adding jwt claims generating method and method to extract id field from jwt

--- src/main/java/pl/rabczynski/openbook/auth/util/JwtUtil.java
+++ src/main/java/pl/rabczynski/openbook/auth/util/JwtUtil.java
@@ -4,23 +4,29 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import pl.rabczynski.openbook.auth.config.JwtProperties;
+import pl.rabczynski.openbook.auth.domain.CustomUserDetails;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class JwtUtil {
 
+    public static final String ID_CLAIM_MAP_KEY = "id";
+    public static final String ROLES_CLAIM_MAP_KEY = "roles";
+
     private final JwtProperties jwtProperties;
 
-    public String generateToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
+    public String generateToken(CustomUserDetails userDetails) {
+        var claims = generateClaims(userDetails);
         return createToken(claims, userDetails.getUsername());
     }
 
@@ -32,13 +38,18 @@ public class JwtUtil {
         return extractClaim(token, Claims::getExpiration);
     }
 
+    public Integer extractUserId(String token) {
+        var idClaim = extractClaim(token, claims -> claims.get(ID_CLAIM_MAP_KEY)).toString();
+        return Integer.valueOf(idClaim);
+    }
+
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
+        var claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
     public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
+        var username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
@@ -57,6 +68,16 @@ public class JwtUtil {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.getExpirationTime() * 1000 * 60))
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey()).compact();
+    }
+
+    private Map<String, Object> generateClaims(CustomUserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        var roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+        claims.put(ID_CLAIM_MAP_KEY, userDetails.getId());
+        claims.put(ROLES_CLAIM_MAP_KEY, roles);
+        return claims;
     }
 
 }
