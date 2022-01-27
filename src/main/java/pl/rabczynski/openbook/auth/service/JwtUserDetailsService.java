5fd044e3cc1538cc7a55ff8bbf092cc8a68ddf54
Filip Rabczynski <f.rabczynski@gmail.com>
2022-01-27 21:59:44 +0100

    replacing returning type with interface implementation instead of interface itself

--- src/main/java/pl/rabczynski/openbook/auth/service/JwtUserDetailsService.java
+++ src/main/java/pl/rabczynski/openbook/auth/service/JwtUserDetailsService.java
@@ -1,7 +1,6 @@
 package pl.rabczynski.openbook.auth.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -15,7 +14,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     private final UserService userService;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userService.getOptionalOfUserByUsername(username)
                 .map(CustomUserDetails::new)
                 .orElseThrow(() -> new UsernameNotFoundException("User with given username not found"));
