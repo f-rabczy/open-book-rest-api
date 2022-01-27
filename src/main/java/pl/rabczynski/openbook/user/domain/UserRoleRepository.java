f7d5360fc06f6dbba9ef7896ca6aefc5a3e01457
Filip Rabczynski <f.rabczynski@gmail.com>
2022-01-27 21:58:59 +0100

    changing interface access modifier

--- src/main/java/pl/rabczynski/openbook/user/domain/UserRoleRepository.java
+++ src/main/java/pl/rabczynski/openbook/user/domain/UserRoleRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import pl.rabczynski.openbook.auth.domain.enums.Role;
 
-public interface UserRoleRepository extends JpaRepository<UserRoleEntity, Integer> {
+interface UserRoleRepository extends JpaRepository<UserRoleEntity, Integer> {
 
     @Query(value = "select u from roles u where u.name =:role")
     UserRoleEntity findByName(Role role);
